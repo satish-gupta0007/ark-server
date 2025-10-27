4e83fd6f03485cbb3b79151655f04d093bc4347e
Satish Gupta <satish.gupta01997@gmail.com>
2025-10-27 18:44:36 +0530

    update nodemailer config

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -3,22 +3,20 @@ import nodeMailer from "nodemailer";
 export const sendEmail = async ({ email, subject, message }) => {
   try {
     const transporter = nodeMailer.createTransport({
-      // host: process.env.SMTP_HOST,       // use for custom SMTP
-      // port: process.env.SMTP_PORT,
-      // secure: false,                     // true if using port 465
-      // auth: {
-      //   user: process.env.SMTP_MAIL,
-      //   pass: process.env.SMTP_PASSWORD,
-      // },
-      service: "SendGrid",
-  auth: {
-    user: "apikey", // this is literally the string "apikey"
-    pass: process.env.SENDGRID_API_KEY,
+      host: process.env.SMTP_HOST,       // use for custom SMTP
+      port: process.env.SMTP_PORT,
+      secure: false,                     // true if using port 465
+      auth: {
+        user: process.env.SMTP_MAIL,
+        pass: process.env.SMTP_PASSWORD,
+      },
+    tls: {
+    rejectUnauthorized: false, // avoid "self signed cert" issues
   },
     });
 
     await transporter.verify();
-
+console.log('transporter.verify();',transporter.verify())
     const options = {
       from: process.env.SMTP_MAIL,
       to: email,
